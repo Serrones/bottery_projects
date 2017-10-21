bb98e17d0cfeab8f12c1e83e2e16b41d8ee542ac
serrones <fabioserrones@gmail.com>
2017-10-21 11:41:28 -0200

    Bottery improvements

--- pingpong/patterns.py
+++ pingpong/patterns.py
@@ -1,11 +1,13 @@
-from bottery.conf.patterns import Pattern
+from bottery.conf.patterns import Pattern, DefaultPattern
 from bottery.views import pong
-from views import hello
+from views import hello, hello_world, not_found
 
 
 
 
 patterns = [
     Pattern('ping', pong),
-    Pattern('hello', hello),
+    Pattern('hi bot', hello),
+    Pattern('hello', hello_world),
+    DefaultPattern(not_found),
 ]
--- pingpong/views.py
+++ pingpong/views.py
@@ -3,3 +3,9 @@ from bottery.message import render
 
 def hello(message):
     return render(message , 'hello.md')
+
+def hello_world(message):
+    return 'Hello World!!'
+
+def not_found(message):
+    return "Sorry, I didn't understand you :/"
